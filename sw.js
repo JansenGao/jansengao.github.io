33a40ffaa3acc50fe17a87e175d9eba35dcb49d8
JansenGao <298066717@qq.com>
2019-12-02 22:34:06 +0800

    Convert to PWA.

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheUrl = [
+    '/base.css',
+    '/france.html'
+  ];
+  const cacheName = 'my-site-cache';
+  
+  // install阶段
+  self.addEventListener('install', (event) => {
+    console.log('sw event: install');
+    event.waitUntil(
+      caches.open(cacheName).then((cache) => {
+        console.log('open cache');
+        return cache.addAll(cacheUrl);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request).then(res => {
+      if (res) {
+        console.log('match');
+        return res;
+      }
+      return fetch(event.request);
+    }));
+  });
